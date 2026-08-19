13a34080d9be1afe0a903aa84f5df2e25c3e8eab
Akash Avaya Behera <112420016+2783tesio@users.noreply.github.com>
2026-08-19 10:00:55 +0530

    Fix GitHub Pages routing with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UserManager from "./components/UserManager";
 import Home from "./components/Home";
 import GivenClothForWash from "./components/GivenClothForWash";
